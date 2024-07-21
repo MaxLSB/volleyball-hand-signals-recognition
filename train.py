399c91cde4345ca8d82f6d9d37253803e9d2a9e1
MaxLSB <m.lasbordes@gmail.com>
2024-07-21 22:19:51 +0200

    small changes

--- train.py
+++ train.py
@@ -1,6 +1,5 @@
 import torch
 import torch.optim as optim
-from sklearn.model_selection import train_test_split
 from torch.utils.data import DataLoader, TensorDataset
 import utils.preProcess as preProcess
 from models.model import DetectionModel
